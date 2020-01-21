d44f930ae74092cc63cf5342c43b31be9090adf2
tOlheiser <tolheiser@outlook.com>
2020-01-20 20:05:01 -0600

    used destructuring and modified classnames

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,15 +3,14 @@ import { Link } from 'react-router-dom';
 import { getItemDate } from '../utils/api.js';
 import { ThemeConsumer } from '../contexts/theme.js';
 
-export default function Item(props) {
-    const {id, url, title, author, time, comments} = props;
+export default function Item({id, url, title, author, time, comments}) {
 
     return(
         <ThemeConsumer>
             {({ theme }) => (
                 <li className="item" key={id}> 
-                    <h3 className="postHeading"><a className={`title title-${theme}`} href={url}>{title}</a></h3>
-                    <p className="info meta">{`by `} 
+                    <h3><a className={`title title-${theme}`} href={url}>{title}</a></h3>
+                    <p className="info">{`by `} 
                         
                         <Link className={`link-${theme}`} to={{
                             pathname: '/user',
