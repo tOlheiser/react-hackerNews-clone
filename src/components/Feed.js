5221967f050249d7b50219ecd90424d0e2fcd094
tOlheiser <tolheiser@outlook.com>
2020-01-20 20:04:34 -0600

    Added a theme consumer, modified class names

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Loading from './Loading';
 import Item from './Item';
 import { getMainFeed } from '../utils/api.js';
+import { ThemeConsumer } from '../contexts/theme';
 
 export default class Feed extends React.Component {
     constructor(props) {
@@ -25,27 +26,31 @@ export default class Feed extends React.Component {
         const { items } = this.state;
 
         return (
-            <div className="container">
-                <div className="flex col container-sm">
-                    <ul>
-                        { items == null ? 
-                            <Loading loading="Loading" />
-                        : items.map((item) => {
-                            return (
-                                <Item 
-                                    id={item.id} 
-                                    url={item.url} 
-                                    title={item.title} 
-                                    author={item.by}
-                                    time={item.time} 
-                                    comments={item.descendants}
-                                    key={item.id}
-                                /> )
-                            })
-                        }
-                    </ul>  
-                </div>
-            </div>
+            <ThemeConsumer>
+                {({theme}) => (
+                    <div className='container'>
+                        <div className="col container-sm">
+                            <ul>
+                                { items == null ? 
+                                    <Loading loading="Loading" />
+                                : items.map((item) => {
+                                    return (
+                                        <Item 
+                                            id={item.id} 
+                                            url={item.url} 
+                                            title={item.title} 
+                                            author={item.by}
+                                            time={item.time} 
+                                            comments={item.descendants}
+                                            key={item.id}
+                                        /> )
+                                    })
+                                }
+                            </ul>  
+                        </div>
+                    </div>                    
+                )}
+            </ThemeConsumer>
         );
     }
 }
